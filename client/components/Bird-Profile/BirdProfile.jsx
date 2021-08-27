b2bdb24e0556dd802a163dce0d529555d0992266
Francisco <fveranicola@gmail.com>
2021-08-27 19:47:30 -0400

    BirdProfile + heatmap file created

---
+++ client/components/Bird-Profile/BirdProfile.jsx
@@ -0,0 +1,13 @@
+import React {  useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const BirdProfile = () => {
+  return (
+    <div>
+      <div>This is a birddddddd</div>
+    </div>
+  );
+}
+
+export default BirdProfile;
