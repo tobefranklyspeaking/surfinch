1b03fbf315a8a3f4d194a9ba1d85afad2f1cf14b
Francisco <fveranicola@gmail.com>
2021-08-28 16:41:06 -0400

    Bird page API endpoints

--- client/components/Bird-Profile/BirdProfile.jsx
+++ client/components/Bird-Profile/BirdProfile.jsx
@@ -5,8 +5,9 @@ const BirdProfile = () => {
   return (
     <div>
       <div>This is a birddddddd</div>
-      <img src='https://images.unsplash.com/photo-1613891188927-14c2774fb8d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80' class="img-fluid. max-width: auto; height: auto;" alt="Responsive image"></img>
-      <h3>Bird Name</h3>
+      <img src='https://images.unsplash.com/photo-1451493683580-9ec8db457610?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FuZGlhbiUyMGdvb3NlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' class="img-fluid. max-width: auto; height: auto;" alt="Responsive image"></img>
+      <h3>Canada Goose</h3>
+      <h5>Branta canadensis</h5>
       <div><p>Bird info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." </p></div>
       <div><p>Notes: I like this bird alot, its really cool! </p></div>
     </div>
@@ -14,3 +15,14 @@ const BirdProfile = () => {
 }
 
 export default BirdProfile;
+
+
+
+//Get Species list for a region
+//https://api.ebird.org/v2/product/spplist/{{regionCode}}
+
+//Get observations of a given species in a given region
+//https://api.ebird.org/v2/data/obs/{{regionCode}}/recent/{{speciesCode}}
+
+//bird information
+//https://api.ebird.org/v2/ref/taxonomy/ebird?species=cangoo
