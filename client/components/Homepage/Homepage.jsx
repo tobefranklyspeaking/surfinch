eb8c04ddd7d319923e9b9bb099994752512cb8a0
shaymarieee <s.marie0431@gmail.com>
2021-08-28 15:43:38 -0400

    layout stuff

--- client/components/Homepage/Homepage.jsx
+++ client/components/Homepage/Homepage.jsx
@@ -3,11 +3,9 @@ import React, { useState, useEffect } from 'react';
 import NavBar from '../Shared/NavBar.jsx';
 
 const Homepage = (props) => {
-  const [birdOfDay, setBirdOfDay] = useState({});
 
   return (
     <div className="home-container d-flex">
-      {/* <NavBar/> */}
       <div className="mini-home-container d-flex flex-column">
         <div className="mini-profile-container"></div>
         <div className="mini-info-container"></div>
@@ -19,4 +17,3 @@ const Homepage = (props) => {
 }
 
 export default Homepage;
-
