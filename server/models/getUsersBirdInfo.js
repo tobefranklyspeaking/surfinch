80d4a2bdce9a21a4f1330da1c0c60808034239e1
Liam Houston <liamhouston@Liams-MacBook-Air.local>
2021-08-30 12:01:06 -0400

    made auto-inc id for schemas user_bird, user_friends

--- server/models/getUsersBirdInfo.js
+++ server/models/getUsersBirdInfo.js
@@ -4,7 +4,7 @@ exports.getUsersBirdInfo = (req, res) => {
   let sql = `SELECT user_birds.bird, user_birds.birdpic_url, user_birds.notes, user_birds.city_sighted, user_birds.state_sighted
              FROM user_birds WHERE userID = ${req.path.userid}`;
   db.query(sql, (err, result) => {
-    if (err) throw err;
+    if (err) console.log(err);
     res.send(result);
   })
 }
