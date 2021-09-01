5f2f118a303658cbd52752bde9b5ed96e9d298e3
Liam Houston <liamhouston@Liams-MacBook-Air.local>
2021-09-01 14:59:59 -0400

    added update route & query

---
+++ server/models/updateBird.js
@@ -0,0 +1,14 @@
+var db = require('../db');
+
+exports.updateBird = (req, res) => {
+  let sql = `UPDATE user_birds
+             SET bird = '${req.body.bird},
+             notes = '${req.body.notes}',
+             city_sighted = ${req.body.city_sighted}',
+             state_sighted = ${req.body.state_sighted}
+             WHERE userID = '${req.body.userID}' and bird = '${req.body.bird}';`;
+  db.query(sql, (err, result) => {
+    if (err) console.log(err);
+    res.send('Bird updated');
+  })
+            }
