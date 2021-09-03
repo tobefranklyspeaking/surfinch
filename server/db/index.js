6d27bdaaae65962839e2a4cf2d012ab41ed7992a
Frank G <fgauss3@gmail.com>
2021-09-03 10:30:16 -0400

    added file to prevent deletion from parent repository

---
+++ server/db/index.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql2');
+
+var connection = mysql.createConnection({
+  user: 'root',
+  password: '', // or whatever your password is
+  database: 'surfinch'
+});
+
+connection.connect(function () {
+  console.log('Connected to db');
+});
+
+module.exports = connection;
