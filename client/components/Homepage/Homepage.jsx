79858ea75a95c34f11df2764962cd3fe9f5b2a61
shaymarieee <s.marie0431@gmail.com>
2021-09-03 01:51:42 -0400

    bhfk

--- client/components/Homepage/Homepage.jsx
+++ client/components/Homepage/Homepage.jsx
@@ -99,8 +99,6 @@ const Homepage = ({ currentUser, location }) => {
       })
   }, [currentUser])
 
-
-
   // console.log('i need some avatars!', currentUser);
 
   return (
@@ -110,7 +108,7 @@ const Homepage = ({ currentUser, location }) => {
           <div className="usericon topbirdersicon">
             <a>
               <Switch><Link to="/user-profile">
-                <Avatar size={75} color={currentUser.avatar_background || '#c8994d'}
+                <Avatar size={100} color={currentUser.avatar_background || '#c8994d'}
                   avatar_pic={currentUser.avatar_pic || 'crane'} />
               </Link></Switch>
             </a>
@@ -120,7 +118,7 @@ const Homepage = ({ currentUser, location }) => {
         </div>
         <div className="mini-map-container">
           {loaded ?  <div className="mini-map-container">
-          <Map styleWidth={40} styleHeight={40} defaultCenter={{ lat: parseFloat(location.lat), lng: parseFloat(location.lng) }} defaultZoom={7} localBirdsMarkers={top10Loc} userMarkers={birdEntries} />
+          <Map styleWidth={40} styleHeight={40} defaultCenter={{ lat: parseFloat(location.lat), lng: parseFloat(location.lng) }} defaultZoom={9} localBirdsMarkers={top10Loc} userMarkers={birdEntries} />
           <div className="birds-nearby-container">
             <LocalBirds top10Birds={top10Birds} location={location} />
           </div>
